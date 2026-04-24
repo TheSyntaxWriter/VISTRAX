6382f2d8c340dcfc0e132ef476048aca563800a2
Prashant Pathak <106474984+TheSyntaxWriter@users.noreply.github.com>
2026-04-24 14:30:09 +0530

    Add files via upload

---
+++ vistrax.js
@@ -0,0 +1,11 @@
+// Smooth scroll
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    if (this.getAttribute("href").startsWith("#")) {
+      e.preventDefault();
+      document
+        .querySelector(this.getAttribute("href"))
+        .scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
